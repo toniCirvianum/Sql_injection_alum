f2b91b176adfac7283fd8dfc34f90789a217f03d
Toni Fernandez <toni.fernandez@cirvianum.cat>
2025-11-18 17:21:15 +0100

    SQL Statement

--- credentials.php
+++ credentials.php
@@ -1,5 +1,5 @@
 <?php
-define('DB_HOST','mysql');
-define('DB_NAME','dbtest');
-define('DB_USER','alum');
-define('DB_PASSWORD','123456');
+// define('DB_HOST','mysql');
+// define('DB_NAME','dbtest');
+// define('DB_USER','alum');
+// define('DB_PASSWORD','123456');
